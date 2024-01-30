38e6b2646045180cc1443b285800a6f6c9b1edc2
xjohnfit <xjohnfitcodes@gmail.com>
2024-01-30 09:38:02 -0500

    CV, Header, CTA, Background

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href="">Download CV</a>
+        <a href="">Contact Talk</a>
+    </div>
+  )
+}
+
+export default CTA
