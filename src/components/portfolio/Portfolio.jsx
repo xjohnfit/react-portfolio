4ab810ee724e413a15c67b8b1b7868a43a3ee88e
John Rocha <xjohnfitcodes@gmail.com>
2024-02-01 20:56:43 -0500

    Services component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <div className="portfolio__item">
+          <div className="portfolio__item-image">
+            <h3>Title</h3>
+            <a href="http://github.com" className='btn'>GitHub</a>
+            <a href="http://github.com" className='btn btn-primary'>Demo</a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
