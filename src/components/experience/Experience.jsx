a2046b50a92f24eb64612045e72bb5cc383fa4c2
xjohnfit <xjohnfitcodes@gmail.com>
2024-02-06 12:19:56 -0500

    Almost done

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -9,6 +9,23 @@ import { BiLogoTailwindCss } from "react-icons/bi";
 import { DiJavascript1 } from "react-icons/di";
 import { FaReact } from "react-icons/fa";
 import { TbBrandAlpineJs } from "react-icons/tb";
+import { SiTypescript } from "react-icons/si";
+import { FaPhp } from "react-icons/fa";
+import { FaLaravel } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { SiPostgresql } from "react-icons/si";
+import { GrOracle } from "react-icons/gr";
+import { SiMongodb } from "react-icons/si";
+import { SiApachecassandra } from "react-icons/si";
+import { FaAws } from "react-icons/fa";
+import { FaGit } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaDocker } from "react-icons/fa6";
+import { FaDev } from "react-icons/fa";
+import { SiApache } from "react-icons/si";
+import { SiNginx } from "react-icons/si";
 
 const Experience = () => {
   return (
@@ -21,64 +38,72 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
+              <TiHtml5 className="experience__details-icon" size={35} />
               <div>
-                <TiHtml5 className="experience__details-icon" size={25} />
+
                 <h4 className="inline">HTML</h4>
                 <br />
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
+              <TiCss3 className="experience__details-icon" size={35} />
               <div>
-                <TiCss3 className="experience__details-icon" size={25} />
+
                 <h4 className="inline">CSS</h4>
                 <br />
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
+              <IoLogoSass className="experience__details-icon" size={35} />
               <div>
-                <IoLogoSass className="experience__details-icon" size={25} />
+
                 <h4 className="inline">SASS</h4>
                 <br />
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
+              <BsFillBootstrapFill className="experience__details-icon" size={35} />
               <div>
-                <BsFillBootstrapFill className="experience__details-icon" size={25} />
+
                 <h4 className="inline">Bootstrap</h4>
                 <br />
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
+              <BiLogoTailwindCss className="experience__details-icon" size={35} />
               <div>
-                <BiLogoTailwindCss className="experience__details-icon" size={25} />
+
                 <h4 className="inline">Tailwind CSS</h4>
                 <br />
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
+              <DiJavascript1 className="experience__details-icon" size={35} />
               <div>
-                <DiJavascript1 className="experience__details-icon" size={25} />
+
                 <h4 className="inline">JavaScript</h4>
                 <br />
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
+              <FaReact className="experience__details-icon" size={35} />
               <div>
-                <FaReact className="experience__details-icon" size={25} />
+
                 <h4 className="inline">React</h4>
                 <br />
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
+              <TbBrandAlpineJs className="experience__details-icon" size={35} />
               <div>
-                <TbBrandAlpineJs className="experience__details-icon" size={25} />
+
                 <h4 className="inline">AlpineJS</h4>
                 <br />
                 <small>Intermediate</small>
@@ -90,65 +115,46 @@ const Experience = () => {
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
+              <SiTypescript className="experience__details-icon" size={35} />
               <div>
-                <TiHtml5 className="experience__details-icon" size={25} />
-                <h4 className="inline">HTML</h4>
+
+                <h4 className="inline">TypeScript</h4>
                 <br />
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
+              <FaPhp className="experience__details-icon" size={35} />
               <div>
-                <TiCss3 className="experience__details-icon" size={25} />
-                <h4 className="inline">CSS</h4>
+
+                <h4 className="inline">PHP</h4>
                 <br />
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
+              <FaLaravel className="experience__details-icon" size={35} />
               <div>
-                <IoLogoSass className="experience__details-icon" size={25} />
-                <h4 className="inline">SASS</h4>
+
+                <h4 className="inline">Laravel</h4>
                 <br />
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
+              <FaNodeJs className="experience__details-icon" size={35} />
               <div>
-                <BsFillBootstrapFill className="experience__details-icon" size={25} />
-                <h4 className="inline">Bootstrap</h4>
+
+                <h4 className="inline">NodeJS</h4>
                 <br />
                 <small>Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
+              <SiExpress className="experience__details-icon" size={35} />
               <div>
-                <BiLogoTailwindCss className="experience__details-icon" size={25} />
-                <h4 className="inline">Tailwind CSS</h4>
-                <br />
-                <small>Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <div>
-                <DiJavascript1 className="experience__details-icon" size={25} />
-                <h4 className="inline">JavaScript</h4>
-                <br />
-                <small>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <div>
-                <FaReact className="experience__details-icon" size={25} />
-                <h4 className="inline">React</h4>
-                <br />
-                <small>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <div>
-                <TbBrandAlpineJs className="experience__details-icon" size={25} />
-                <h4 className="inline">AlpineJS</h4>
+
+                <h4 className="inline">Express</h4>
                 <br />
                 <small>Intermediate</small>
               </div>
@@ -159,72 +165,48 @@ const Experience = () => {
           <h3>Databases</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <GrMysql className="experience__details-icon" size={35} />
               <div>
-                <h4>HTML</h4>
+                <h4>MySQL</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <SiPostgresql className="experience__details-icon" size={35} />
               <div>
-                <h4>CSS</h4>
+                <h4>PostGres</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <GrOracle className="experience__details-icon" size={35} />
               <div>
-                <h4>SAAS</h4>
+                <h4>Oracle</h4>
                 <small>Intermediate</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <SiMongodb className="experience__details-icon" size={35} />
               <div>
-                <h4>Bootstrap</h4>
+                <h4>MongoDB</h4>
                 <small>Intermediate</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <SiMongodb className="experience__details-icon" size={35} />
               <div>
-                <h4>Tailwind CSS</h4>
+                <h4>MongoDB Atlas</h4>
                 <small>Intermediate</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <SiApachecassandra className="experience__details-icon" size={35} />
               <div>
-                <h4>JavaScript</h4>
+                <h4>Cassandra</h4>
                 <small>Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>React</h4>
-                <small>Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>JavaScript</h4>
-                <small>Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>AlpineJS</h4>
-                <small>Intermediate</small>
               </div>
             </article>
           </div>
@@ -233,74 +215,59 @@ const Experience = () => {
           <h3>Technologies</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <FaAws className="experience__details-icon" size={35} />
               <div>
-                <h4>HTML</h4>
+                <h4>AWS</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <FaGit className="experience__details-icon" size={35} />
               <div>
-                <h4>CSS</h4>
+                <h4>Git</h4>
                 <small>Experienced</small>
               </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <FaGithub className="experience__details-icon" size={35} />
               <div>
-                <h4>SAAS</h4>
+                <h4>GitHub</h4>
                 <small>Intermediate</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <FaDocker className="experience__details-icon" size={35} />
               <div>
-                <h4>Bootstrap</h4>
+                <h4>Docker</h4>
                 <small>Intermediate</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <FaDev className="experience__details-icon" size={35} />
               <div>
-                <h4>Tailwind CSS</h4>
+                <h4>DevOps</h4>
                 <small>Intermediate</small>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <SiApache className="experience__details-icon" size={35} />
               <div>
-                <h4>JavaScript</h4>
-                <small>Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>React</h4>
-                <small>Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>JavaScript</h4>
-                <small>Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>AlpineJS</h4>
+                <h4>Apache</h4>
                 <small>Intermediate</small>
               </div>
             </article>
+
+            <article className="experience__details">
+              <SiNginx className="experience__details-icon" size={35} />
+              <div>
+                <h4>NGINX</h4>
+                <small>Intermediate</small>
+              </div>
+            </article>
+
           </div>
         </div>
       </div>
